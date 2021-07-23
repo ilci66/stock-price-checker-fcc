514da24cc8ba3df77d9e3c7879cee1b7ab551157
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-07-23 12:36:04 +0200

    initial commit

---
+++ model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const stockSchema = new mongoose.Schema({
+  stock: String,
+  likes: {type: String, default:[]}
+});
+
+const Stock = mongoose.model('Stock', stockSchema)
