caa59eb2eb45f38a1d56c22cd6143259d25f909c
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-07-25 00:46:06 +0200

    2 out 5 tests correct

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,14 +6,41 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  test("one stock get", (done) =>{
-    const like = "false"
-    const stock = "goog"
+  // test("one stock get", (done) =>{
+  //   const checkbox = "false"
+  //   const stock = "goog"
+  //   chai.request(server)
+  //     .get(`/api/stock-prices/?stock=${stock}&like=${checkbox}`)
+  //     .end((err, res) => {
+  //       console.log(res)
+  //       // assert.equal(res.stock, "GOOG")
+  //       done()
+  //     })
+  // })
+  test('GET req one stock', (done) => {
     chai.request(server)
-      .get(`/api/stock-prices/?stock=${stock}&like=${checkbox}`)
+      .get('/api/stock-prices/')
+      //I keep forgetting to set the header
+      .set("content-type", "application/json")
+      .query({ stock: "GOOG"})
       .end((err, res) => {
-        assert.equal(res.stock, "GOOG")
+        assert.equal(res.status, 200)
+        //it's not directly res it's carried in body 
+        assert.equal(res.body.stockData.stock, "GOOG")
+        done();
+      })
+  });
+  test('Viewing one stock and liking it', (done) => {
+    chai.request(server)
+      .get('/api/stock-prices/')
+      .set("content-type", "application/json")
+      .query({stock: "GOOG", like:"true"})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.stock, "GOOG")
+        assert.isAbove(res.body.stockData.likes, 0)
         done()
       })
   })
+
 });
